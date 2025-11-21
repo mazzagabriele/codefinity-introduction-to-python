71d615c82e458e08ce22f9865860389cd4f9d280
mazzagabriele <>
2025-11-21 18:11:02 +0000

    Tuple Operations

---
+++ other_data_types/tuple_operations/main.py
@@ -0,0 +1,20 @@
+# Initial items on shelf #1 (provided as a tuple)
+shelf1 = ("celery", "spinach", "cucumbers")
+
+# Items being added to the shelf #1 (provided as a list)
+shelf1_update = ["tomatoes", "celery", "cilantro"]
+
+#conversione in tuple
+shelf1_update_tuple = tuple(shelf1_update)
+
+#tuple concatenati
+shelf1_concat =  shelf1 + shelf1_update_tuple
+
+#conteggi e indici
+celery_count = shelf1_concat.count("celery")
+celery_index = shelf1_concat.index("celery")
+
+#stampe
+print("Updated Shelf #1: ", shelf1_concat)
+print ("Number of Celery: ", celery_count)
+print ("Celery Index: ", celery_index)
